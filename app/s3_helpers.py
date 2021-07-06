5f7285f82cb523a24cf12852cda765e8adc26736
Chris Regan <chrisreg007@gmail.com>
2021-07-06 17:35:40 -0400

    aws working

--- app/s3_helpers.py
+++ app/s3_helpers.py
@@ -5,12 +5,13 @@ import uuid
 
 BUCKET_NAME = os.environ.get("S3_BUCKET")
 S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
-ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
+ALLOWED_EXTENSIONS = {"pdf", "docx"}
 
 s3 = boto3.client(
-    "s3",
+    service_name="s3",
     aws_access_key_id=os.environ.get("S3_KEY"),
-    aws_secret_access_key=os.environ.get("S3_SECRET")
+    aws_secret_access_key=os.environ.get("S3_SECRET"),
+    endpoint_url=S3_LOCATION,
 )
 
 
@@ -26,8 +27,13 @@ def get_unique_filename(filename):
 
 
 def upload_file_to_s3(file, acl="public-read"):
+    print(os.environ.get("S3_SECRET"))
+    print(os.environ.get("S3_KEY"))
+    print(BUCKET_NAME)
+    print("response:",s3.list_buckets())
     try:
-        s3.upload_fileobj(
+        print('filename:',file.filename )
+        print("response:",s3.upload_fileobj(
             file,
             BUCKET_NAME,
             file.filename,
@@ -35,9 +41,13 @@ def upload_file_to_s3(file, acl="public-read"):
                 "ACL": acl,
                 "ContentType": file.content_type
             }
-        )
+        ))
     except Exception as e:
+        print("EXCEPTION:", str(e))
         # in case the our s3 upload fails
+        print("S3 upload failed")
         return {"errors": str(e)}
 
     return {"url": f"{S3_LOCATION}{file.filename}"}
+
+ # reference: https://hackmd.io/@jpshafto/SyWY45KGu
--- react-app/src/components/Application/index.js
+++ react-app/src/components/Application/index.js
@@ -5,16 +5,60 @@ import { useHistory, useParams } from "react-router-dom";
 import {get_one_application} from "../../store/application"
 function Application(){
     const { appId } = useParams();
-
+    const [file,setFile] = useState(null)
+    const [uploadSuccess, setUploadSuccess] = useState(false);
+    const [fileLoading, setFileLoading] = useState(false)
+    const [useDefault, setUseDefault] = useState(false)
     const dispatch = useDispatch();
     useEffect (()=>{
         dispatch(get_one_application(appId))
     },[])
     let application = useSelector(state => state.application.one_application);
 
+    const handleFileSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("resume",file)
+        setFileLoading(true);
+
+        const res = await fetch(`/api/application/resume/add/${appId}/resume`, {
+            method: "POST",
+            body: formData,
+        });
+        if (res.ok) {
+            await res.json();
+            setFileLoading(false);
+        }
+        else {
+            setFileLoading(false);
+            // a real app would probably use more advanced
+            // error handling
+            console.log("error");
+        }
+
+
+
+    }
+
+    const updateFile = (e) => {
+        const file = e.target.files[0];
+        setFile(file);
+    }
     return (<div>
         {application.job_title}
+        <form onSubmit={handleFileSubmit}>
+            <input
+              type="file"
+              accept=".pdf,.docx"
+              onChange={updateFile}
+            />
+            {/* <input type='checkbox' value={useDefault} onChange={e=>setUseDefault(e.target.checked)}>Use your Default Resume</input> */}
+            <button type="submit">Submit</button>
+            {(fileLoading)&& <p>Loading...</p>}
+        </form>
     </div>)
 }
 
 export default Application;
+
+//reference: https://hackmd.io/@jpshafto/SyWY45KGu
