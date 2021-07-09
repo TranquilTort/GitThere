fd2f0ac5402b90fe2defad199048bd4735c394cc
Chris Regan <chrisreg007@gmail.com>
2021-07-09 17:29:20 -0400

    file downloading staged

--- react-app/src/components/Application/index.js
+++ react-app/src/components/Application/index.js
@@ -11,32 +11,32 @@ function Application(){
     const { appId } = useParams();
     const history = useHistory();
     const [pageLoaded, setPageLoaded] = useState(false);
-
+    const [fileType, setFileType] = useState("resume");
     const [file,setFile] = useState(null);
     const [uploadSuccess, setUploadSuccess] = useState(false);
     const [fileLoading, setFileLoading] = useState(false);
-    const [useDefault, setUseDefault] = useState(false);
     const [showNotesForm, setShowNotesForm] = useState(false);
     const dispatch = useDispatch();
     useEffect (async ()=>{
         await dispatch(get_one_application(appId))
         await dispatch(get_all_notes(appId))
         dispatch(authenticate());
-    },[])
+    },[fileLoading])
     let application = useSelector(state => state.application.one_application);
     let notes = useSelector(state => state.note.notes);
     let user = useSelector(state => state.session.user);
+    console.log('THE CURRENT APPLICATION',application)
     useEffect (() => {
 
-    },[notes])
+    },[notes,fileLoading])
     console.log("ALL NOTES:", notes)
     const handleFileSubmit = async (e) => {
         e.preventDefault();
         const formData = new FormData();
-        formData.append("resume",file)
+        formData.append(fileType,file)
         setFileLoading(true);
 
-        const res = await fetch(`/api/application/resume/add/${appId}/resume`, {
+        const res = await fetch(`/api/application/document/add/${appId}/${fileType}`, {
             method: "POST",
             body: formData,
         });
@@ -68,6 +68,9 @@ function Application(){
             history.push('/')
         }
     }
+    function handleFileDownload(awsUrl) {
+        console.log('INSIDE HANDLE DOWNLOAD',awsUrl)
+    }
     return (
     <div className="app-page-container">
         <div className="app-info-container">
@@ -76,6 +79,7 @@ function Application(){
             </div>
             <div className="app-job-title">
                 {application.job_title}
+                <a  className="app-job-link" href={`${application.url_link}`} target="_blank">Go To Link</a>
             </div>
             <div className="app-status">
                 Status: <select className="app-status-select" onChange={e=>dispatch(moveStatus(e.target.value,application.id,user.id))}>
@@ -90,30 +94,46 @@ function Application(){
             </div>
             <div className="app-edit-btns-container">
                 <button className="edit-app-btn">Edit Application Info</button>
-
                 <button className="delete-app-btn" onClick={handleDelete}>Remove Application</button>
             </div>
-
-
             <div className="app-decription-label">
             Description:
             </div>
-
             <div className="app-description">
-
                 {application.job_description}
             </div>
 
         </div>
-        <form onSubmit={handleFileSubmit}>
-            <label>Upload the Resume You applied with</label>
+        {/* reference: https://hackmd.io/@jpshafto/SyWY45KGu */}
+        <div className="file-download-container">
+            <div>
+                Your files here:
+            </div>
+            <div>
+                {application.resume? <div>Your resume has been uploaded: <button onClick={e=>handleFileDownload(application.resume)} >Download Resume</button> </div>: "No resume uploaded"}
+            </div>
+            <div>
+                {application.cover_letter? <div>Your Cover Letter Has been uploaded <button onClick={e=>handleFileDownload(application.cover_letter)} >Download Cover Letter</button> </div>: "No cover letter uploaded"}
+            </div>
+            <div>
+                {application.cv?<div>Your CV has been uploaded <button onClick={e=>handleFileDownload(application.cv)} >Download CV</button> </div>: "No cv uploaded"}
+            </div>
+        </div>
+        <form className="file-upload-form" onSubmit={handleFileSubmit}>
+            <label>Upload Documents</label>
+            <select value={fileType}className="file-type-select" onChange={e=>{setFileType(e.target.value)}}>
+                <option value='resume'>Resume</option>
+                <option value='cv'>CV</option>
+                <option value='cover_letter'>Cover Letter</option>
+            </select>
             <input
+              className = 'upload-selection'
               type="file"
               accept=".pdf,.docx"
               onChange={updateFile}
             />
-            {/* <input type='checkbox' value={useDefault} onChange={e=>setUseDefault(e.target.checked)}>Use your Default Resume</input> */}
-            <button type="submit">Upload</button>
+
+            <button className='upload-file-btn' type="submit">Upload</button>
             {(fileLoading)&& <p>Loading...</p>}
         </form>
         {showNotesForm ?<button style={{backgroundColor:'#F6E0ED'}} onClick={toggleForm} className="toggle-notes-btn">Add a note</button> :<button style={{backgroundColor:'#d8d9db'}} onClick={toggleForm} className="toggle-notes-btn">Add a note</button> }
@@ -125,5 +145,3 @@ function Application(){
 }
 
 export default Application;
-
-//reference: https://hackmd.io/@jpshafto/SyWY45KGu
